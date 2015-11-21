4a50e273214d9515a14be1ed8cefd8a9ee0fd8b4
stuart <stuart@xnet.com>
2015-11-20 21:11:18 -0600

    Adding sketch example file from timed_call_backs

--- examples/fire/fire.ino
+++ examples/fire/fire.ino
@@ -1,33 +1,23 @@
 #include <Fire.h>
 #include <Time.h>
 
-// Assign pin 9 to the red LED.
-Fire fire_red(9);
+// Assign pin 9 to the yellow LED.
+Fire fire_yellow(9);
+// Assign pin 10 to the red LED.
+Fire fire_red(10);
 // Assign pin 10 to the gren LED.
-Fire fire_green(10);
+Fire fire_green(11);
 
 Time time;
 
 void setup()
 {
-  fire_red.begin();
-  fire_green.begin(FIRE_NORMAL_FLICKER, BEACON_MIN, BEACON_MAX, FIRE_BEACON);
-
-  time.speed((uint16_t)4, doit);
-  time.speed((uint16_t)5, doitto); 
+	fire_yellow.begin();
+	fire_red.begin(FIRE_FAST_BEACON, BEACON_MIN, BEACON_MAX, FIRE_BEACON);
+	fire_green.begin(FIRE_SLOW_BEACON, BEACON_MIN, BEACON_MAX, FIRE_BEACON);
 }
 
 void loop()
 {
-   time.run();
-}
-
-void doit()
-{
-  fire_red.burn();
-}
-
-void doitto()
-{
-  fire_green.burn();
+	time.run();
 }

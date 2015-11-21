9f0a433e0f8b6a1d155cebeaf1845197ee0eda6b
stuart <stuart@xnet.com>
2015-11-20 21:08:13 -0600

    This time we are really adding the Time.cpp and Time.h files

--- Time.cpp
+++ Time.cpp
@@ -3,16 +3,15 @@
 #include "Arduino.h"
 
 // Constructor.
-Time::Time(){}
 
 // Destructor.
-Time::~Time(){/*nothing to destruct*/}
 
-void Time::speed(uint16_t delay_ms, timer_callback function_name) 
+void Time::speed(uint16_t delay_ms, timer_callback function_name, void * formal_inst) 
 {
 	callback_delay[timer_count] = delay_ms;
 	callbacks[timer_count] = function_name;
 	time_last_called[timer_count] = millis();
+	inst[i] = formal_inst
 	timer_count++;
 }
 
@@ -20,22 +19,32 @@ void Time::run()
 {
 	uint8_t i;
 	uint16_t time_now;
+	uint16_t time_next_call;
 
-	time_now = millis();
+	time_now = 0xffff & millis();
 
-	for (i = 0; i < timer_count; i++) 
+	for(i = 0; i < timer_count; i++)
 	{
-		if (time_now - time_last_called[i] >= callback_delay[i]) 
+		time_next_call = 0xffff & (time_last_called[i] + callback_delay[i]);
+		if(time_now < time_old[i])
 		{
-			time_last_called[i] += callback_delay[i];
-			(*callbacks[i])();
+			time_last_called[1] = time_now;
+		}
+		time_old[i] = time_now;
+		if(time_next_call < time_now)
+		{
+			if(time_now > (time_next_call + callback_delay[i]))
+			{
+				time_last_called[i] = (0x0ffff & time_now);
+	}
+			else
+			{
+				time_last_called[i] = time_next_call;
+}
+
+			(*(callback[i]))(inst[i]);
 		}
 	}
 }
-
-
-
-
-
 
 
--- Time.h
+++ Time.h
@@ -9,18 +9,20 @@ class Time
 {
 	public:
 	
-	Time();
-	~Time();
+	Time(){};
+	~Time(){};
 	
-	void speed(uint16_t delay_ms, timer_callback function_name);
+	void speed(uint16_t delay_ms, timer_callback function_name, void * formal_inst);
 	void run();
 	
 	private:
 	uint8_t timer_count;
 	
-	uint8_t callback_delay[10];
+	uint16_t callback_delay[10];
     timer_callback callbacks[10];
     uint16_t time_last_called[10];
+	void * inst[10];
+	uint16_t time_old;
 };
 
 #endif
